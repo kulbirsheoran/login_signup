1b3fd707ee4402774ca0b4ef2388af0336a17788
kulbirsheoran <92265109+kulbirsheoran@users.noreply.github.com>
2021-10-10 22:16:13 +0530

    login&signup

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Login & SignUp"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(decoration: InputDecoration(
+              hintText: "Please input name"
+            )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
